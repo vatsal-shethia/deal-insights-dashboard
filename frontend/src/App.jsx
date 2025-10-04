d9823a7cb29da597f4e5b9c6899ece4f2d070f95
anannayamustcode <anannayareallycodes@gmail.com>
2025-10-04 22:24:13 +0530

    ai integrated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import Upload from './components/Upload';
 import Dashboard from './components/Dashboard';
 import CompareDeals from './components/CompareDeals';
 import History from './components/History';
-// import DealList from './components/DealList';
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
         <Route path="/dashboard/:dealId" element={<Dashboard />} />
         <Route path="/compare" element={<CompareDeals />} />
         <Route path="/history" element={<History />} />
-        {/* <Route path="/history" element={<DealList />} /> */}
         {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
       </Routes>
     </Router>
